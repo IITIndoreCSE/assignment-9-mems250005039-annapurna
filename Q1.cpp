0d2a8a733c527eac9df1cf6c5316ea839371c1d7
mems250005039-annapurna <mems250005039@iiti.ac.in>
2025-10-17 16:35:08 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+struct Item {
+    int id;
+    float cost;
+};
+
+int main() {
+    Item inventory[6] = {
+        {101, 75.50f},
+        {102, 35.00f},
+        {103, 90.25f},
+        {104, 50.00f}
+    };
+    
+    std::cout << "Items with cost greater than 50.00:" << std::endl;
+    for (int i = 0; i < 6; ++i) {
+        if (inventory[i].cost > 50.00f) {
+            std::cout << "ID: " << inventory[i].id << ", Cost: " << inventory[i].cost << std::endl;
+        }
+    }
+    
+    return 0;
+}
