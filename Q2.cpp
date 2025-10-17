2ff0b662cffa0603978893f8751964f790e2d4b3
mems250005039-annapurna <mems250005039@iiti.ac.in>
2025-10-17 16:54:15 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+struct colour{
+int red;
+int blue;
+int green;
+};
+int main() {
+  colour palette[5] = {
+{22,0,0},
+{0,22,0},
+{0,0,22},
+{22,22,22},
+{0,0,0},
+};
 
+    for (int i = 0; i < 3; ++i) {
+        palette[i].red = 22 - palette[i].red;
+        palette[i].green = 22 - palette[i].green;
+        palette[i].blue = 22 - palette[i].blue;
+    }
+    
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Element " << i << ": "
+                  << "R=" << palette[i].red
+                  << ", G=" << palette[i].green
+                  << ", B=" << palette[i].blue << std::endl;
+    }
+
+    return 0;
+}
